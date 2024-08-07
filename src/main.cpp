947ac1836883ddebf4b1e913fbb63fb449edc14e
DonizeteVida <donizetevida1998@outlook.com>
2024-08-07 06:06:17 -0300

    Basic LVGL+TFT_ESPI setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,89 +1,134 @@
-// Animates white pixels to simulate flying through a star field
+/*Using LVGL with Arduino requires some extra steps:
+ *Be sure to read the docs here: https://docs.lvgl.io/master/get-started/platforms/arduino.html  */
 
-#include <SPI.h>
+#include <lvgl.h>
+
+#if LV_USE_TFT_ESPI
 #include <TFT_eSPI.h>
+#endif
 
-// Use hardware SPI
-TFT_eSPI tft = TFT_eSPI();
+/*To use the built-in examples and demos of LVGL uncomment the includes below respectively.
+ *You also need to copy `lvgl/examples` to `lvgl/src/examples`. Similarly for the demos `lvgl/demos` to `lvgl/src/demos`.
+ *Note that the `lv_examples` library is for LVGL v7 and you shouldn't install it for this version (since LVGL v8)
+ *as the examples and demos are now part of the main LVGL library. */
 
-// With 1024 stars the update rate is ~65 frames per second
-#define NSTARS 1024
-uint8_t sx[NSTARS] = {};
-uint8_t sy[NSTARS] = {};
-uint8_t sz[NSTARS] = {};
+//#include <examples/lv_examples.h>
+//#include <demos/lv_demos.h>
 
-uint8_t za, zb, zc, zx;
+/*Set to your screen resolution*/
+#define TFT_HOR_RES   320
+#define TFT_VER_RES   240
 
-// Fast 0-255 random number generator from http://eternityforest.com/Projects/rng.php:
-uint8_t __attribute__((always_inline)) rng()
+/*LVGL draw into this buffer, 1/10 screen size usually works well. The size is in bytes*/
+#define DRAW_BUF_SIZE (TFT_HOR_RES * TFT_VER_RES / 10 * (LV_COLOR_DEPTH / 8))
+uint32_t draw_buf[DRAW_BUF_SIZE / 4];
+
+#if LV_USE_LOG != 0
+void my_print( lv_log_level_t level, const char * buf )
 {
-  zx++;
-  za = (za^zc^zx);
-  zb = (zb+za);
-  zc = ((zc+(zb>>1))^za);
-  return zc;
+    LV_UNUSED(level);
+    Serial.println(buf);
+    Serial.flush();
+}
+#endif
+
+/* LVGL calls it when a rendered image needs to copied to the display*/
+void my_disp_flush( lv_display_t *disp, const lv_area_t *area, uint8_t * px_map)
+{
+    /*Copy `px map` to the `area`*/
+
+    /*For example ("my_..." functions needs to be implemented by you)
+    uint32_t w = lv_area_get_width(area);
+    uint32_t h = lv_area_get_height(area);
+
+    my_set_window(area->x1, area->y1, w, h);
+    my_draw_bitmaps(px_map, w * h);
+     */
+
+    /*Call it to tell LVGL you are ready*/
+    lv_disp_flush_ready(disp);
 }
 
-void setup() {
-  za = random(256);
-  zb = random(256);
-  zc = random(256);
-  zx = random(256);
+/*Read the touchpad*/
+void my_touchpad_read( lv_indev_t * indev, lv_indev_data_t * data )
+{
+    /*For example  ("my_..." functions needs to be implemented by you)
+    int32_t x, y;
+    bool touched = my_get_touch( &x, &y );
 
-  Serial.begin(9600);
-  tft.init();
-  tft.setRotation(1);
-  tft.fillScreen(TFT_BLACK);
+    if(!touched) {
+        data->state = LV_INDEV_STATE_RELEASED;
+    } else {
+        data->state = LV_INDEV_STATE_PRESSED;
 
-  // fastSetup() must be used immediately before fastPixel() to prepare screen
-  // It must be called after any other graphics drawing function call if fastPixel()
-  // is to be called again
-  //tft.fastSetup(); // Prepare plot window range for fast pixel plotting
+        data->point.x = x;
+        data->point.y = y;
+    }
+     */
+}
+
+void setup()
+{
+    String LVGL_Arduino = "Hello Arduino! ";
+    LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
+
+    Serial.begin( 115200 );
+    Serial.println( LVGL_Arduino );
+
+    lv_init();
+
+    /*Set a tick source so that LVGL will know how much time elapsed. */
+    // lv_tick_set_cb(millis);
+
+    /* register print function for debugging */
+#if LV_USE_LOG != 0
+    lv_log_register_print_cb( my_print );
+#endif
+
+    lv_display_t * disp;
+#if LV_USE_TFT_ESPI
+    /*TFT_eSPI can be enabled lv_conf.h to initialize the display in a simple way*/
+    disp = lv_tft_espi_create(TFT_HOR_RES, TFT_VER_RES, draw_buf, sizeof(draw_buf));
+#else
+    /*Else create a display yourself*/
+    disp = lv_display_create(TFT_HOR_RES, TFT_VER_RES);
+    lv_display_set_flush_cb(disp, my_disp_flush);
+    lv_display_set_buffers(disp, draw_buf, NULL, sizeof(draw_buf), LV_DISPLAY_RENDER_MODE_PARTIAL);
+#endif
+
+    /*Initialize the (dummy) input device driver*/
+    lv_indev_t * indev = lv_indev_create();
+    lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER); /*Touchpad should have POINTER type*/
+    lv_indev_set_read_cb(indev, my_touchpad_read);
+
+    /* Create a simple label
+     * ---------------------
+     lv_obj_t *label = lv_label_create( lv_scr_act() );
+     lv_label_set_text( label, "Hello Arduino, I'm LVGL!" );
+     lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );
+
+     * Try an example. See all the examples
+     *  - Online: https://docs.lvgl.io/master/examples.html
+     *  - Source codes: https://github.com/lvgl/lvgl/tree/master/examples
+     * ----------------------------------------------------------------
+
+     lv_example_btn_1();
+
+     * Or try out a demo. Don't forget to enable the demos in lv_conf.h. E.g. LV_USE_DEMOS_WIDGETS
+     * -------------------------------------------------------------------------------------------
+
+     lv_demo_widgets();
+     */
+
+    lv_obj_t *label = lv_label_create( lv_scr_act() );
+    lv_label_set_text( label, "Hello Arduino, I'm LVGL!" );
+    lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );
+
+    Serial.println( "Setup done" );
 }
 
 void loop()
 {
-  unsigned long t0 = micros();
-  uint8_t spawnDepthVariation = 255;
-
-  for(int i = 0; i < NSTARS; ++i)
-  {
-    if (sz[i] <= 1)
-    {
-      sx[i] = 160 - 120 + rng();
-      sy[i] = rng();
-      sz[i] = spawnDepthVariation--;
-    }
-    else
-    {
-      int old_screen_x = ((int)sx[i] - 160) * 256 / sz[i] + 160;
-      int old_screen_y = ((int)sy[i] - 120) * 256 / sz[i] + 120;
-
-      // This is a faster pixel drawing function for occasions where many single pixels must be drawn
-      tft.drawPixel(old_screen_x, old_screen_y,TFT_BLACK);
-
-      sz[i] -= 2;
-      if (sz[i] > 1)
-      {
-        int screen_x = ((int)sx[i] - 160) * 256 / sz[i] + 160;
-        int screen_y = ((int)sy[i] - 120) * 256 / sz[i] + 120;
-  
-        if (screen_x >= 0 && screen_y >= 0 && screen_x < 320 && screen_y < 240)
-        {
-          uint8_t r, g, b;
-          r = g = b = 255 - sz[i];
-          tft.drawPixel(screen_x, screen_y, tft.color565(r,g,b));
-        }
-        else
-          sz[i] = 0; // Out of screen, die.
-      }
-    }
-  }
-  unsigned long t1 = micros();
-  //static char timeMicros[8] = {};
-
- // Calculate frames per second
-  Serial.println(1.0/((t1 - t0)/1000000.0));
+    lv_task_handler(); /* let the GUI do its work */
+    delay(5); /* let this time pass */
 }
-
-
